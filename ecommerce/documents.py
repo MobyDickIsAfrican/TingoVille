5e18491e0c4a591154f73b57769ee633bc0bcf4a
Luvo <50812180+MobyDickIsAfrican@users.noreply.github.com>
2020-06-04 03:04:18 +0200

    Update documents.py

--- ecommerce/documents.py
+++ ecommerce/documents.py
@@ -1,7 +1,6 @@
 from elasticsearch_dsl import connections
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-#from .models import Product, Shop, ProductCategory
 from django_elasticsearch_dsl import Document
 from django_elasticsearch_dsl.registries import registry
 from .models import Product, ProductCategory, Shop
@@ -40,42 +39,4 @@ class CategoryDocument(Document):
         model = ProductCategory
         fields = ['CategoryName']
 
-'''
-@index.document
-class ProductDocument(Document):
-    #snowball uses stop words, standard tokenizer
-    ProductType = Text()
-    Name = Text()
-    Description = Text()
 
-    class Index:
-        name = 'products'
-        settings = {'number_of_shards' :1, 'number_of_replicas' : 1}
-
-    def save(self, **kwargs):
-        super(ProductDocument, self).save(self, **kwargs)
-
-@shop_index.document
-class ShopDocument(Document):
-    Name = Text(analyzer = 'snowball')
-
-    class Index:
-        name = 'shops'
-        settings = {'number_of_shards': 2, 'number_of_replicas' : 1 }
-    def save(self, **kwargs):
-        super(ShopDocument, self).save(self, **kwargs)
-
-class CategoryDocument(Document):
-    Name = Text(analyzer = 'english')
-
-    class Index:
-        name = 'categories'
-        settings = {'number_of_shards': 1, 'number_of_replicas': 1}
-
-    def save(self, **kwargs):
-        super(CategoryDocument, self).save(self, **kwargs)
-
-ShopDocument.init()
-s = ShopDocument(Name = 'Zando', meta = {'id': 150})
-s.save()
-'''
